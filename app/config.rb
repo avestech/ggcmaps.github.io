356c88aeb6f28c157b5d8adad6b0f52622528511
Mike Deiters <miked49er@gmail.com>
2016-10-11 23:38:55 -0400

    Setup automation scripts

---
+++ app/config.rb
@@ -0,0 +1,6 @@
+require "susy"
+css_dir = 'public/css'
+sass_dir = 'lib/sass'
+javascripts_dir = 'public/js'
+output_style = :expanded
+relative_assets = true;
---
+++ app/gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      files: ["lib/client-js/*.js"],
+      options: {
+        esnext: true,
+        globals: {
+          jQuery: true
+        } // globals
+      } // options
+    }, // jshint
+    uglify: {
+      options: {
+        mangle: {
+          except: [
+            '$timeout',
+            '$eval'
+          ]
+        } // mangle
+      }, // options
+      my_target: {
+        files: {
+          'public/js/script.min.js': ['lib/client-js/*.js']
+        } // files
+      } // my_target
+    }, // uglify
+    compass: {
+      dev: {
+        options: {
+          config: 'config.rb'
+        } // options
+      } // dev
+    }, // compass
+    watch: {
+      options: { livereload: true },
+      scripts: {
+        files: ['lib/client-js/*.js'],
+        tasks: ['uglify', 'jshint']
+      }, // scripts
+      sass: {
+        files: ['lib/sass/*.scss'],
+        tasks: ['compass:dev']
+      }, // sass
+      html: {
+        files: ['templates/*.html']
+      } // html
+    } // watch
+  }); // initConfig
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+
+  grunt.registerTask('js', ['uglify']);
+  grunt.registerTask('css', ['compass:dev']);
+  grunt.registerTask('default', ['watch']);
+};
