3e851302b2d6d1b19214c21dffd91d2c9dd99440
Mike Deiters <miked49er@gmail.com>
2016-10-20 21:39:41 -0400

    Added open/close menu functions

--- app/lib/client-js/nav.js
+++ app/lib/client-js/nav.js
@@ -1,10 +1,30 @@
+function openMenu() {
+  var menu = document.getElementsByClassName('nav')[0];
+  var content = document.getElementsByClassName('content-disable')[0];
+
+  if (menu.className === 'nav') {
+    menu.classList.add('nav-active');
+    content.classList.add('active');
+  }
+} // openMenu
+
+function closeMenu() {
+  var menu = document.getElementsByClassName('nav')[0];
+  var content = document.getElementsByClassName('content-disable')[0];
+
+  if (menu.className === 'nav nav-active') {
+    menu.classList.remove('nav-active');
+    content.classList.remove('active');
+  }
+} //closeMenu
+
 function diningSubMenu() {
   var menu = document.getElementsByClassName('dining')[0];
 
   if (menu.className === 'dining') {
-    menu.classList.add('nav-active');
+    menu.classList.add('dining-active');
   }
-  else if (menu.className === 'dining nav-active') {
-    menu.classList.remove('nav-active');
+  else if (menu.className === 'dining dining-active') {
+    menu.classList.remove('dining-active');
   }
 } // diningSubMenu
