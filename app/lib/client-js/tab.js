207412af327d1dececde5a17fefbc913629c5a26
Mike Deiters <miked49er@gmail.com>
2016-11-29 03:15:08 -0500

    Built tabContentHeight function to set the height of tab-content

--- app/lib/client-js/tab.js
+++ app/lib/client-js/tab.js
@@ -4,6 +4,9 @@ function openTab(element) {
 
   if (!tab.classList.contains('tab-active')) {
     tab.classList.toggle('tab-active');
+    var height = tabContentHeight(element);
+    var tabcontent = tab.childNodes[0].childNodes[7];
+    tabcontent.style.height = height + 'px';
   }
 }
 
@@ -13,4 +16,10 @@ function closeTab(element) {
   if (tab.classList.contains('tab-active')) {
     tab.classList.toggle('tab-active');
   }
+}
+
+function tabContentHeight(element) {
+  var clientHeight = (window.innerHeight || document.body.clientHeight);
+  var title = document.getElementById(element).childNodes[0].childNodes[1].clientHeight;
+  return clientHeight - title;
 }
