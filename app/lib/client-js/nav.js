afb525945340f276d0d8084b88a471fa8d5dbcd5
Mike Deiters <miked49er@gmail.com>
2016-12-04 12:54:09 -0500

    Finished commenting nav.js

--- app/lib/client-js/nav.js
+++ app/lib/client-js/nav.js
@@ -1,28 +1,38 @@
+// Open the menu
 function openMenu() {
+  // The menu itself
   var menu = document.getElementsByClassName('nav')[0];
+  // The content overlay
   var content = document.getElementsByClassName('content-disable')[0];
 
   if (menu.className === 'nav') {
     menu.classList.add('nav-active');
+    // Disables the content of the page until the menu is closed
     content.classList.add('active');
   }
-} // openMenu
+}
 
+// Close the menu
 function closeMenu() {
+  // The menu itself
   var menu = document.getElementsByClassName('nav')[0];
+  // The content overlay
   var content = document.getElementsByClassName('content-disable')[0];
 
   if (menu.className === 'nav nav-active') {
     menu.classList.remove('nav-active');
+    // Enables the content of the page again
     content.classList.remove('active');
   }
 
+  // When you close the menu also close the floors dropdown
   var dd = document.getElementsByClassName('floors')[0];
   if (dd.className === 'floors floors-active') {
     dd.classList.remove('floors-active');
   }
-} //closeMenu
+}
 
+// Open/Close the dining submenu
 function diningSubMenu() {
   var menu = document.getElementsByClassName('dining')[0];
 
@@ -32,4 +42,4 @@ function diningSubMenu() {
   else if (menu.className === 'dining dining-active') {
     menu.classList.remove('dining-active');
   }
-} // diningSubMenu
+}
